07299139726f827cc230e90563c84a7e93a11bef
Brian Moore <mooreb>
2013-11-11 18:00:04 -0800

    separate mangled gc lines

--- root/apps/apache/htdocs/AdminGCViz/remote-data-collection/gcdotlog_one_event_per_line.pl
+++ root/apps/apache/htdocs/AdminGCViz/remote-data-collection/gcdotlog_one_event_per_line.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl -w
 
-# $Id: //depot/cloud/rpms/nflx-webadmin-gcviz/root/apps/apache/htdocs/AdminGCViz/remote-data-collection/gcdotlog_one_event_per_line.pl#2 $
-# $DateTime: 2013/05/15 18:34:23 $
-# $Change: 1838706 $
+# $Id: //depot/cloud/rpms/nflx-webadmin-gcviz/root/apps/apache/htdocs/AdminGCViz/remote-data-collection/gcdotlog_one_event_per_line.pl#3 $
+# $DateTime: 2013/11/05 12:09:10 $
+# $Change: 2024106 $
 # $Author: mooreb $
 
 use strict;
@@ -28,6 +28,18 @@ while(<STDIN>) {
         # starting a new record
         flush_previous_record($previous_record);
         $previous_record = $line;
+    }
+    elsif($line =~ m/(^.*[0-9]+[.][0-9]+ secs])([0-9]+[.][0-9]+: .*$)/) {
+        # two records are conflated 
+        $previous_record = $previous_record . $1;
+        flush_previous_record($previous_record);
+        $previous_record = $2;
+    }
+    elsif($line =~ m/(^.*)([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.*)/) {
+        # two records (with -XX:+PrintGCDateStamps) are conflated 
+        $previous_record = $previous_record . $1;
+        flush_previous_record($previous_record);
+        $previous_record = $2;
     }
     else {
         # append to the previous record
